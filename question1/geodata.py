b28e70a6206929cb211b95e23b4dda92a2075db4
Jason Goodell <jgoodell77@gmail.com>
2014-04-30 20:34:10 -0400

    Slightly more sophisticated demo script with some error handling.

--- question1/geodata.py
+++ question1/geodata.py
@@ -22,19 +22,65 @@ HOST = "api.sba.gov"
 
 URI = "/geodata/city_county_links_for_state_of/%s.json"
 
-if __name__ == "__main__":
-    from pprint import pprint
-    from optparse import OptionParser as P
-    
-    connection = HTTPConnection(HOST, 80)
+CONNECTION = HTTPConnection(HOST, 80)
 
-    for state in STATES:
-        state = state.lower()
-        json_file = open("data/%s.json" % state, 'w')
-        connection.request("GET", URI % state)
-        response = connection.getresponse()
-        json_data = json.loads(response.read())
-        json_file.write(json.dumps(json_data, sort_keys=True,
-                                   indent=4, separators=(',', ': ')))
-        json_file.close()
-        connection.close()
+def fetch_data(uri, state, file_name, file_uri=None):
+    '''Fetches the provided state data and places the data in a specified file.
+
+    Arguments:
+    state:      Two capital letter state abbreviation, e.g. CA for California.
+    file_name:  A string indicating the name of the file to place the data in.
+
+    Optional Arguments
+    file_uri:   The location to place the file at, default is the current directory.
+    '''
+    state = state.lower()
+    try:
+        json_file = open(file_uri + '/' + file_name, 'w')
+    except TypeError:
+        json_file = open(file_name, 'w')
+    CONNECTION.request("GET", uri % state)
+    response = CONNECTION.getresponse()
+    json_data = json.loads(response.read())
+    json_file.write(json.dumps(json_data, sort_keys=True,
+                               indent=4, separators=(',', ': ')))
+    json_file.close()
+    CONNECTION.close()
+    
+
+if __name__ == "__main__":
+    import sys
+    import os
+    from optparse import OptionParser as P
+    p = P(description="Fetches geolocation data by state from api.sba.gov.",
+          version='0.1-alpha',
+          prog=os.path.basename(__file__),
+          usage="python %prog [options]",
+          epilog="This file can also be used as a module.")
+    p.add_option('-v', '--verbose', action='store_true', dest="verbose",
+                 help='Will print out the state currently being fetched to stdout.')
+    p.add_option('-S', '--state', action='store', dest='state',
+                 help='Select a state to fetch geolocation data about using the two capital letter abbreviation, e.g. CA for California.')
+    opts, args = p.parse_args()
+
+    if opts.state:
+        if opts.verbose: print("Fetching %s..." % opts.state)
+        try:
+            fetch_data(URI, opts.state, '%s.json' % opts.state, file_uri='data')
+        except ValueError:
+            print("Error: '%s' is not a valid state abbreviation." % opts.state)
+            sys.exit(1)
+        except Exception, e:
+            print("Error: Check your internet connection.")
+            sys.exit(1)
+        sys.exit(0)
+    else:
+        print('here')
+        for state in STATES:
+            if opts.verbose: print("Fetching %s..." % state)
+            try:
+                fetch_data(URI, state, '%s.json' % state, file_uri='data')
+            except:
+                print("Error: Check your internet connection.")
+                sys.exit(1)
+        sys.exit(0)
